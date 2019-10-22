a651ccef5051c6a96378cb97391d2be736611be1
marcnjaramillo <marcn.jaramillo@gmail.com>
2019-10-22 00:09:02 -0700

    Set up association and allow for image

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,6 @@
 class Account < ApplicationRecord
+  has_many :posts
+  has_one_attached :profile_image
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
