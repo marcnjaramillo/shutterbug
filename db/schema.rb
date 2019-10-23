e33f1b01ccb87f3ca745cb10f781bd9efd6a9bbf
marcnjaramillo <marcn.jaramillo@gmail.com>
2019-10-23 01:33:15 -0700

    Add profile fields to account table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_23_062401) do
+ActiveRecord::Schema.define(version: 2019_10_23_082834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 2019_10_23_062401) do
     t.string "first_name", limit: 20
     t.string "last_name", limit: 20
     t.string "username", limit: 20
+    t.text "description"
+    t.string "website"
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
   end
