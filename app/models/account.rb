504b89246f21080a83dc97f3d273aab2b0ed1828
marcnjaramillo <marcn.jaramillo@gmail.com>
2019-10-23 01:32:41 -0700

    Create profile route and view

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,13 +1,22 @@
 class Account < ApplicationRecord
-  has_many :posts
-  has_one_attached :profile_image
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :posts
+  has_one_attached :profile_image
+
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def total_followers
+    0
+  end
+
+  def total_following
+    0
   end
   
 end
