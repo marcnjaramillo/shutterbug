8bf585f51e1ba80df44d0b713fa9d622279c07b3
marcnjaramillo <marcn.jaramillo@gmail.com>
2019-10-23 22:38:23 -0700

    Order posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,9 +4,15 @@ class Post < ApplicationRecord
 
   before_create :set_active
 
+  default_scope { order(created_at: :desc) }
+
   scope :active, -> { where active: true }
 
   def set_active
     self.active = true
+  end
+
+  def total_likes
+    0
   end
 end
