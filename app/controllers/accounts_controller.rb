ab441c00fa2f7df3bf72a3a4cb1222da5b755b21
marcnjaramillo <marcn.jaramillo@gmail.com>
2019-10-29 00:21:07 -0700

    Add follower info to index and create follow method

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -4,6 +4,9 @@ class AccountsController < ApplicationController
 
   def index
     @posts = Post.active
+    following_ids = Follower.where(follower_id: current_account.id).map(&:following_id)
+    following_ids << current_account.id
+    @follower_suggestions = Account.where.not(id: following_ids)
   end
 
   def profile
@@ -12,6 +15,16 @@ class AccountsController < ApplicationController
 
   def set_account
     @account = Account.find_by_username(params[:username])
+  end
+
+  def follow_account
+    follower_id = params[:follow_id]
+    if Follower.create!(follower_id: current_account.id, following_id: follower_id)
+      flash[:success] = "Now following a new user"
+    else
+      flash[:danger] = "Unable to follow"
+    end
+    redirect_to dashboard_path
   end
 
   private
