4423144d45f8fd5a78751b24df13eaf3c1ca9cdb
marcnjaramillo <marcn.jaramillo@gmail.com>
2019-11-05 21:14:37 -0800

    Add return path to comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,8 @@ class CommentsController < ApplicationController
     @comment.account_id = current_account.id if account_signed_in?
 
     if @comment.save
-      redirect_to dashboard_path, flash: { success: "Comment was created successfully!"}
+      return_url = params[:comment][:return_to].present? ? post_path(@comment.post_id) : dashboard_path
+      redirect_to return_url, flash: { success: "Comment was created successfully!"}
     else
       redirect_to new_comment_path, flash: { danger: "Comment was not saved!"}
     end
