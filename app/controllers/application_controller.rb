47e1b4ed21601ca55c5db0c01e6d9339f1fcdf0b
marcnjaramillo <marcn.jaramillo@gmail.com>
2019-11-12 15:23:31 -0800

    Redirect to dashboard after sign up or sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,12 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name, :email, :password, :profile_image, :website, :description])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :last_name, :email, :password, :profile_image, :website, :description])
   end
+
+  def after_sign_up_path_for(resource)
+    dashboard_path
+  end
+
+  def after_sign_in_path_for(resource)
+    dashboard_path
+  end
 end
